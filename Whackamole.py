6a8d25559d29eefa225b812bf90074d16ad8baf4
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-11-03 23:06:15 -0400

    Updated from Brython Server: 11/3/2017 11:06:15 PM

--- Whackamole.py
+++ Whackamole.py
@@ -5,22 +5,6 @@
 from ggame import *
 from random import randint
 
-def mouseClick(event):
-    if event.x <= 100 and event.y <= 100:
-        print('Morgan')
-    if event.x >= 100 and event.x <=200 and event.y <= 100:
-        print('Jack')
-    if event.x >=200 and event.x <=300 and event.y <=100:
-        print('Pedro')
-    if event.x <= 100 and event.y >= 100 and event.y <= 200:
-        print('Deigo')
-    if event.x >= 100 and event.x <=200 and event.y >= 100 and event.y <= 200:
-        print('Greg')
-    if event.x >=200 and event.x <=300 and event.y >= 100 and event.y <= 200:
-        print('Charlie')
-    data['frames'] = 0
-
-
 def moveCircle():
     num = randint(1,6)
     if num == 1:
@@ -49,7 +33,23 @@ def moveCircle():
         Sprite(regCircle,(250,150))
         
     data['frames'] = 0
-
+    
+def mouseClick(event):
+    num = randint(1,6)
+    if event.x <= 100 and event.y <= 100:
+        print('Score')
+    if event.x >= 100 and event.x <=200 and event.y <= 100:
+        print('Score')
+    if event.x >=200 and event.x <=300 and event.y <=100:
+        print('Score')
+    if event.x <= 100 and event.y >= 100 and event.y <= 200:
+        print('Score')
+    if event.x >= 100 and event.x <=200 and event.y >= 100 and event.y <= 200:
+        print('Score')
+    if event.x >=200 and event.x <=300 and event.y >= 100 and event.y <= 200:
+        print('Score')
+        
+    data['frames'] = 0
 
 def step():
     data['frames'] += 1
@@ -61,7 +61,6 @@ if __name__ == '__main__':
     data = {}
     data['score'] = 0
     data['frames'] = 0
-
 
 red = Color(0xFF0000,1)
 black = Color(0x000000,1)
